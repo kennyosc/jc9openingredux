1d8653ec57708f180e2e6d703fc6edc961bfd947
kennyosc <kennyoscar95@gmail.com>
2019-06-18 11:32:12 +0700

    add index.js reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,39 @@
-
+//Reducer ini adalah yang divisi2 yang akan menerima form data dari Dispatch (officer (bank))
 
 //REDUCER : pada dasarnya adalah sebuah function
 
 const songsReducer = () => {
-    
+    return [
+        {
+            title: 'Forever  Young',
+            artist: 'Black Pink',
+            duration: '4:05',
+            personnel: ['Lisa','Kim Jennie','Kim Ji','Rose']
+        },
+        {
+            title: 'Lucid Dream',
+            artist: 'Monogram',
+            duration: '3:09',
+            personnel: ['Kevin','Riwon']
+        },
+        {
+            title: "Let's not fall in love",
+            artist: 'Big-Bang',
+            duration: '4:20',
+            personnel: ['Seungri']
+        },
+        {
+            title: 'Forever  Young',
+            artist: 'Black Pink',
+            duration: '4:05',
+            personnel: ['Lisa','Kim Jennie','Kim Ji','Rose']
+        },
+        {
+            title: 'Forever  Young',
+            artist: 'Black Pink',
+            duration: '4:05',
+            personnel: ['Lisa','Kim Jennie','Kim Ji','Rose']
+        },
+
+    ]
 }
