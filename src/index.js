b94c6061c70bd8f62e5863e7bd67b2e928d64461
kennyosc <kennyoscar95@gmail.com>
2019-06-18 12:10:28 +0700

    add redux storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+//ini yang akan menghubungkan redux dengan react
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
+//kalau .js dapat tidak ditulis
 import App from './components/App.js'
+import reducers from './reducers/index.js'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+
+//function yang digunakan untuk membuat storage/store/state
+var STORE = createStore(reducers)
+
+//STORE yang di atas akan dimasukkan ke dalam APP dengan menggunakan PROVIDER (yang akan menghubungkan react dengan redux)
+ReactDOM.render(
+    <Provider store = {STORE}>
+        <App/>
+    </Provider>
+    , document.getElementById('root')
+)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,39 +1,56 @@
+//menggabungkan semua REDUCERS menjadi 1
+import {combineReducers} from 'redux'
+
 //Reducer ini adalah yang divisi2 yang akan menerima form data dari Dispatch (officer (bank))
 
 //REDUCER : pada dasarnya adalah sebuah function
 
+//songsReducer adalah reducer untuk DATA
+//ingat di dalam ilustrasi bahwa nanti REDUCER akan menerima 2 hal, yaitu ACTION dan DATA LAMA
 const songsReducer = () => {
     return [
         {
-            title: 'Forever  Young',
+            title: 'Forever Young',
             artist: 'Black Pink',
             duration: '4:05',
-            personnel: ['Lisa','Kim Jennie','Kim Ji','Rose']
+            personnel: ['Lisa', 'Kim Jennie', 'Kim Ji', 'Rose']
         },
         {
             title: 'Lucid Dream',
-            artist: 'Monogram',
+            artist: "Monogram" ,
             duration: '3:09',
-            personnel: ['Kevin','Riwon']
+            personnel: ["Kevin", "Riwon"] 
         },
         {
-            title: "Let's not fall in love",
-            artist: 'Big-Bang',
+            title: 'Let\'s not fall in love',
+            artist: "Big-Bang" ,
             duration: '4:20',
-            personnel: ['Seungri']
+            personnel: ["Seungri", "Taeyang", "Daesung", "G-Dragon", "T.O.P"]
         },
         {
-            title: 'Forever  Young',
-            artist: 'Black Pink',
-            duration: '4:05',
-            personnel: ['Lisa','Kim Jennie','Kim Ji','Rose']
+            title: 'Save Me',
+            artist: "BTS" ,
+            duration: '2:12',
+            personnel: ["V", "Jung-Kook", "Ji-Min", "Suga", "Jin", "RM", "J-Hope"]
         },
         {
-            title: 'Forever  Young',
-            artist: 'Black Pink',
-            duration: '4:05',
-            personnel: ['Lisa','Kim Jennie','Kim Ji','Rose']
-        },
-
+            title: 'Blood, Sweat, and Tears',
+            artist: "Bangtan Boys" ,
+            duration: '5:15',
+            personnel: ["V", "Jung-Kook", "Ji-Min", "Suga", "Jin", "RM", "J-Hope"]
+        }
     ]
+}
+
+//synthax nya adalah combineReducers akan menerima sebuah object
+//combineReducers adalah object storagenya. dimana nanti akan menghasilkan sebuah STATE
+//export default combineReducers akan mempermudah combineReducers untuk di export langsung
+export default combineReducers({
+    //synthax adalah key(nama tempat penyimpanan) : namaReducer
+    songs:songsReducer
+})
+
+//ini adalah function untuk memberikan REDUCER sebuah ACTION dan DATA LAMA
+const selectedSongReducer = (action,selectedSong) => {
+
 }
